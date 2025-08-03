f5be12cfab8f45a96ed4ba4c35ad8c6b0f89ebbe
AlexEmanuelSoulis <ebalexsoulis@outlook.com>
2025-08-03 19:08:40 -0400

    fizzbuzz completed

--- script.js
+++ script.js
@@ -29,3 +29,70 @@ function FizzBuzz(num){
 }
 
 FizzBuzz(100);
+
+console.log("------------------------------")
+
+/*
+Part 2: Prime Time
+Now we will move onto something slightly more complex.
+Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1.
+ For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. 
+ Similarly, the numbers 7 and 11 are prime. 
+ As numbers become larger, determining whether or not they are prime is increasingly difficult,
+ but loops make this process relatively easy!
+Write a script that accomplishes the following:
+Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, starting at n and incrementing from there.
+As soon as you find the prime number, log that number and exit the loop.
+Continuing with the example above, if n is equal to 4, your loop should log 5. 
+Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. 
+Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+*/
+
+
+// function findNextPrimeNumber(userStartNumb){
+//     let i = 100
+//     while(i < userStartNumb ){
+//         for(let i = 1; i <= userStartNumb; i--){
+//             if(userStartNumb % i === 0 || userStartNumb / userStartNumb !== 1){
+//                 continue;
+//             }
+//         }
+//     }
+//     if(userStartNumb % i === 1 || userStartNumb / userStartNumb === 1){
+//         console.log(`The next prime number is ${userStartNumb}`);
+//     }
+// }
+
+// findNextPrimeNumber(7)
+
+
+
+
+
+let i = 1;
+let startNumber = 7;
+while(i <  200){
+    let isPrimeNumber = true;
+    i++;
+    for(let y = startNumber; y < i; y++){
+        if(startNumber % y === 0 || startNumber / startNumber !== 1){
+            isPrimeNumber = false;
+            continue;
+        } 
+       else{
+        let inc = startNumber + 1
+        while( inc > 200){
+            inc++;
+            if(startNumber % y !== 0 && startNumber / startNumber === 1){
+                console.log(`The prime number is: ${inc}`);
+                continue;
+            }
+        }
+       }
+        
+
+        console.log(startNumber);
+        
+    }
+}
